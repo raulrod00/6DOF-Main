503f7655e603083d318613087d08110cdf76669d
Raul Rodriguez <raulrod00@yahoo.com>
2021-02-24 11:31:03 -0800

    Initial Commit
    Initial Commit/Publish

---
+++ Assets/Plugins/UdpOutSim.cs
@@ -0,0 +1,125 @@
+﻿//////////////////////////////////////////////////////////////
+// Unity Game Udp Out Sim PlugIn For PlayBox 
+// QQ: 2642789658
+// Version: 20190520
+//////////////////////////////////////////////////////////////
+
+//-----------------------------------------------------------
+// Using System Package
+//-----------------------------------------------------------
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Runtime.InteropServices;
+
+//-----------------------------------------------------------
+// Using Unity & DLL Package
+//-----------------------------------------------------------
+using UnityEngine;
+using OutSimData;
+
+//-----------------------------------------------------------
+// UdpOutSim Class Block
+//-----------------------------------------------------------
+public class UdpOutSim : MonoBehaviour {
+
+	//-----------------------------------------------------------
+	// Public Vars
+	//-----------------------------------------------------------
+	public string ipString = "192.168.1.101";// Motion box IP is 192.168.15.201? What's App
+
+	//-----------------------------------------------------------
+	// Private Vars
+	//-----------------------------------------------------------
+	// PlayBox Target Machine
+	IPEndPoint 	IpEndHost; 		// Host IP End
+	Socket  	UdpSocket; 		// Udp Socket
+
+	// Game Rigidbody Component
+	Rigidbody rb;
+
+	// Game OutSimData Component
+	OutSimDataClass OutSimDataUtils = new OutSimDataClass();
+
+
+	//-----------------------------------------------------------
+	// Use this for initialization
+	//-----------------------------------------------------------
+	void Start () {
+
+		// Get IP End Host Point
+		IpEndHost = new IPEndPoint(IPAddress.Parse(ipString), 10222 );
+		
+		//IpEndHost = new IPEndPoint(IPAddress.Parse("192.168.1.101"), 10222 );
+		//IpEndHost = new IPEndPoint(IPAddress.Parse("192.168.43.98"), 10222 );
+		//IpEndHost = new IPEndPoint(IPAddress.Parse("255.255.255.255"), 10222 );
+		//IpEndHost = new IPEndPoint(IPAddress.Broadcast, 10222 );
+
+		// Udp Socket 
+		UdpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+		// Get RigidBodgy Component
+		rb = GetComponent<Rigidbody> ();
+
+		// OutSimData Initialize
+		OutSimDataUtils.OutSimData_Init ();
+
+	} // End: void Start () 
+		
+
+	//-----------------------------------------------------------
+	// Update is called once per frame
+	//-----------------------------------------------------------
+	void Update () {
+
+		// Get Target Body Velocity & EulerAngles
+		OutSimDataUtils.BodyVelocity 	 = rb.velocity;
+		OutSimDataUtils.BodyEulerAngles  = transform.eulerAngles;
+
+		Debug.Log(OutSimDataUtils.BodyVelocity); // Raul
+		// OutSimData Update
+		OutSimDataUtils.OutSimData_Update();
+
+
+		// Get Udp Send Message
+		byte[] UdpSendByte = OutSimDataUtils.GameSimData.GetBytes();
+
+		// Transmit Udp Send Message
+		UdpSocket.SendTo(UdpSendByte, UdpSendByte.Length, SocketFlags.None, IpEndHost);
+
+	}
+	//===========================================================
+		
+
+	/*
+	//-----------------------------------------------------------
+	// Update is called once per Fix Time
+	//-----------------------------------------------------------
+	void FixedUpdate () {
+
+
+
+	} // End:  void FixedUpdate ()  */
+	//==========================================================
+		
+
+
+   //----------------------------------------------------------
+   // MonoBehaviour.OnApplicationQuit()
+   // Sent to all game objects before the application quits.
+   //----------------------------------------------------------
+   void OnApplicationQuit()
+    {
+		
+		// Udp Socket Close
+		if ( UdpSocket != null )
+			UdpSocket.Close();
+        
+	} // End: OnApplicationQuit()
+	//========================================================
+    
+}
---
+++ Assets/Scripts/MoveSphere.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveSphere : MonoBehaviour
+{
+
+    private Rigidbody rb;
+    public float speed;// MetersPerFrame;
+    public float distanceInMeters;
+    private float speedMetersPerSec;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        // 1cup flour, 1 tsp salt, 2/3 milk, 2 eggs
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //speedMetersPerSec = speedMetersPerFrame * Time.deltaTime;
+
+        float mH = Input.GetAxis("Horizontal") * speed;
+        float mV = Input.GetAxis("Vertical") * speed;
+
+        //mH *= Time.deltaTime;
+        //mV *= Time.deltaTime;
+
+        //transform.Translate(mH, 0, mV);
+
+        rb.velocity = new Vector3(mH * speed, rb.velocity.y, mV * speed);
+    }
+}
